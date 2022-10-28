01512f29f8be3634c096e0cdecd6429036dc9316
Fermin Verdolini <ferminverdolini@gmail.com>
2022-10-28 13:26:05 -0300

    Next signal and prev signal

---
+++ tp_final/Debug/src/Stack.d
@@ -0,0 +1,5 @@
+src/Stack.o src/Stack.d: ../src/Stack.c \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/tp_final/inc/Stack.h \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/tp_final/inc/Tecla.h
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/tp_final/inc/Stack.h:
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/tp_final/inc/Tecla.h:
--- tp_final/Debug/src/tp_final.d
+++ tp_final/Debug/src/tp_final.d
@@ -1,25 +1,25 @@
 src/tp_final.o src/tp_final.d: ../src/tp_final.c \
- /home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_dac.h \
- /home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/inc/LPC17xx.h \
- /home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/inc/core_cm3.h \
- /home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/inc/core_cmInstr.h \
- /home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/inc/core_cmFunc.h \
- /home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/inc/system_LPC17xx.h \
- /home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc_types.h \
- /home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_timer.h \
- /home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_pinsel.h \
- /home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_gpio.h \
- /home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_exti.h \
- /home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_gpdma.h
-/home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_dac.h:
-/home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/inc/LPC17xx.h:
-/home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/inc/core_cm3.h:
-/home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/inc/core_cmInstr.h:
-/home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/inc/core_cmFunc.h:
-/home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/inc/system_LPC17xx.h:
-/home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc_types.h:
-/home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_timer.h:
-/home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_pinsel.h:
-/home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_gpio.h:
-/home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_exti.h:
-/home/avig/Documents/MCUXpresso_11.6.0_8187/TRABAJO_FINAL/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_gpdma.h:
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_dac.h \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/inc/LPC17xx.h \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/inc/core_cm3.h \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/inc/core_cmInstr.h \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/inc/core_cmFunc.h \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/inc/system_LPC17xx.h \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc_types.h \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_timer.h \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_pinsel.h \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_gpio.h \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_exti.h \
+ /home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_gpdma.h
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_dac.h:
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/inc/LPC17xx.h:
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/inc/core_cm3.h:
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/inc/core_cmInstr.h:
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/inc/core_cmFunc.h:
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/inc/system_LPC17xx.h:
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc_types.h:
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_timer.h:
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_pinsel.h:
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_gpio.h:
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_exti.h:
+/home/ferminverdolini/Universidad/Octavo\ semestre/Electronica\ digital\ 3/Trabajo\ Final/Sintetizador/CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_gpdma.h:
